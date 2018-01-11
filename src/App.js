bad390f53fe005b18ade57e82030285ec41e9f40
majunchang <2471978285@qq.com>
2018-01-11 22:36:10 +0800

    学习react-redux以及修饰器的写法

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import React from 'react'
 // 为了解决这种强耦合的问题  我们统一采用参数传递的方式
 // import {plusGunF, minusGunF} from "./actions";
 
+// Connect 负责从外部获取组件需要的参数
+import {connect} from 'react-redux'
+import {plusGunF, minusGunF, addGunAsync} from "./actions";
+
 class App extends React.Component {
     render() {
-        console.log(this);
         console.log(this.props);
-        const store = this.props.store;
-        const num = store.getState();
+        const num = this.props.num;
         const plusGunF = this.props.plusGunF;
         const minusGunF = this.props.minusGunF;
         const addGunAsync = this.props.addGunAsync;
         return (
             <div>
                 <h1>现在有机枪{num}把</h1>
-                <button onClick={() => store.dispatch(plusGunF())}>申请武器</button>
-                <button onClick={()=>store.dispatch(minusGunF())}>减少武器</button>
-                <button onClick={()=>store.dispatch(addGunAsync())}>拖欠两天再给上交</button>
+                <button onClick={plusGunF}>申请武器</button>
+                <button onClick={minusGunF}>减少武器</button>
+                <button onClick={addGunAsync}>拖欠两天再给上交</button>
             </div>
         )
     }
 }
 
-export default  App
+// App需要的属性
+const marStateToProps = (state) => {
+    return {num: state}
+}
+// App需要的方法的集合
+const actionCreators = {plusGunF, minusGunF, addGunAsync}
+// connect包裹App
+App = connect(marStateToProps, actionCreators)(App)
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
 import App from './App'
-import {counter, plusGunF, minusGunF, addGunAsync} from "./actions";
+// 我们安装好 react-redux包之后  provider组件在应用的最外层 差传入store 只用一次
+import {Provider} from 'react-redux'
+import {counter} from "./actions";
 
 
-const store = createStore(counter, applyMiddleware(thunk))
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ?  window.devToolsExtension() : f=>f
+));
 
-function render() {
+
     ReactDOM.render(
-        <App store={store} plusGunF={plusGunF} minusGunF={minusGunF} addGunAsync={addGunAsync}/>,
+        (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        ),
         document.getElementById('root')
     )
-}
 
 
-store.subscribe(render)
-render()
