0180cbbb36cb8ab8141c9f584e3ad92eafd66558
majunchang <smart1626>
2018-01-11 19:04:45 +0800

    初始redux

--- src/App.js
+++ src/App.js
@@ -1,94 +1,25 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
-
 import React from 'react'
-// 按需加载antd-mobile组件
-import {Button,List} from 'antd-mobile'
-import 'antd-mobile/dist/antd-mobile.css'
+// 为了解决这种强耦合的问题  我们统一采用参数传递的方式
+// import {plusGunF, minusGunF} from "./actions";
 
 class App extends React.Component {
     render() {
-        const boss = '李云龙'
-        return (
-            <div>
-                <h2>独立团现在的团长是{boss}</h2>
-                <One lao='张大彪'></One>
-                <Qi lao='孙德胜'></Qi>
-            </div>
-        )
-    }
-}
-
-function Qi(props) {
-    return <h2>骑兵连连长{props.lao},冲啊</h2>
-}
-
-class One extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            soliders: ['小虎', '小柱', '王根生']
-        }
-        //  1 在构造函数中 bind  this
-          this.addSolider = this.addSolider.bind(this)
-    }
-
-    addSolider() {
-        this.setState({
-            soliders:[...this.state.soliders,'新兵蛋子'+Math.random()]
-        })
-    }
-    //  3 直接用箭头函数生命
-    // addSolider = (() => {
-    //     console.log(this)
-    // })
-
-    render() {
         console.log(this);
         console.log(this.props);
+        const store = this.props.store;
+        const num = store.getState();
+        const plusGunF = this.props.plusGunF;
+        const minusGunF = this.props.minusGunF;
+        const addGunAsync = this.props.addGunAsync;
         return (
             <div>
-                <h2>一营营长是{this.props.lao}</h2>
-                <Button type='primary' onClick={this.addSolider}>新兵入伍，抓壮丁</Button>
-                {/*  2 直接用箭头函数绑定 */}
-                {/*<button onClick={()=>this.addSolider()}>新兵入伍，抓壮丁</button>*/}
-                <List
-                    renderHeader={()=>'士兵列表'}
-                >
-                    {this.state.soliders.map((v) => {
-                        return <List.Item key={v} arrow='horizontal'>
-                            {v}
-                            </List.Item>
-                    })}
-                </List>
-                {/*<ul>*/}
-                    {/*{this.state.soliders.map((v) => {*/}
-                        {/*return <li key={v}>{v}</li>*/}
-                    {/*})}*/}
-                {/*</ul>*/}
+                <h1>现在有机枪{num}把</h1>
+                <button onClick={() => store.dispatch(plusGunF())}>申请武器</button>
+                <button onClick={()=>store.dispatch(minusGunF())}>减少武器</button>
+                <button onClick={()=>store.dispatch(addGunAsync())}>拖欠两天再给上交</button>
             </div>
         )
     }
 }
 
-export default App;
+export default  App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import App from './App'
+import {counter, plusGunF, minusGunF, addGunAsync} from "./actions";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const store = createStore(counter, applyMiddleware(thunk))
+
+function render() {
+    ReactDOM.render(
+        <App store={store} plusGunF={plusGunF} minusGunF={minusGunF} addGunAsync={addGunAsync}/>,
+        document.getElementById('root')
+    )
+}
+
+
+store.subscribe(render)
+render()
